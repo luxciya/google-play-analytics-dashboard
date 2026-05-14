2b0d8dcd01166975b00237f6e6eed07cc59b1711
luxciya <asviniluxci@gmail.com>
2026-05-14 19:43:48 +0530

    Delete internship_dashboard_project directory

--- internship_dashboard_project/utils/translations.py
+++
@@ -1,8 +0,0 @@
-translations = {
-    'Beauty': 'सौंदर्य',
-    'Business': 'வணிகம்',
-    'Dating': 'Partnersuche',
-    'Travel & Local': 'Voyage et Local',
-    'Productivity': 'Productividad',
-    'Photography': '写真'
-}
